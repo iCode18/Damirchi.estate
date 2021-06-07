45cf43cc0485815dd4e880c5468d019b0335168a
Afshin Alavi <afshinalavi@Afshins-MacBook-Pro.local>
2021-06-07 23:45:45 +0200

    temp

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,11 @@ const observer = new ResizeObserver((entries) => {
     ]('limited')
   }
 })
-
-ps.forEach((p) => {
-  observer.observe(p)
+const ps = document.querySelectorAll('.collaps')
+const observer = new ResizeObserver((entries) => {
+  for (let entry of entries) {
+    entry.target.classList[
+      entry.target.scrollHeight > entry.contentRect.height ? 'add' : 'remove'
+    ]('limited2')
+  }
 })
